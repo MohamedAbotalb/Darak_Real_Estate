ef6527c5777ed5fc37b8abb1709cbd180235e09f
Nada Alnajdi <nada.abdo.alnajdi@gmail.com>
2024-05-19 13:51:53 +0300

    users,report_users,report_properties and images tables

--- Backend/database/migrations/2014_10_12_000000_create_users_table - Copy.php
+++ Backend/database/migrations/2014_10_12_000000_create_users_table - Copy.php
@@ -13,14 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('avatar');
-            $table->integer('phone_number');
-            $table->enum('role', ['admin', 'user', 'landlord']);
             $table->rememberToken();
             $table->timestamps();
         });
