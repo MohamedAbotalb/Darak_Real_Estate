2bd05969d02297559951c5e4c35b8590f72a8214
dohaseif2 <dohaseif2@gmail.com>
2024-05-25 23:22:26 +0300

    feat(database):Add models an fillable

---
+++ Backend/app/Models/Property.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'slug', 'description', 'num_of_rooms', 'num_of_bathrooms', 'area', 'price', 'location_id', 'property_type_id', 'user_id', 'availability', 'listing_type'];
+
+}
---
+++ Backend/app/Models/Tour.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'property_id', 'status'];
+}
