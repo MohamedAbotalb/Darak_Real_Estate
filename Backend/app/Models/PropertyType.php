13910110fdaa8cd76122da7f66d9744b71122130
Nada Alnajdi <nada.abdo.alnajdi@gmail.com>
2024-06-17 03:30:42 +0300

    add relation between propertyType and Property

--- Backend/app/Models/PropertyType.php
+++ Backend/app/Models/PropertyType.php
@@ -4,9 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class PropertyType extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'slug'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($propertyType) {
+            $propertyType->slug = Str::slug($propertyType->name);
+        });
+
+        static::updating(function ($propertyType) {
+            $propertyType->slug = Str::slug($propertyType->name);
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function properties()
+    {
+        return $this->hasMany(Property::class);
+    }
 }
