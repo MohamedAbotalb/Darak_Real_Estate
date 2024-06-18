6290dfc98a62bd8355cfb93d3391db52e8fae117
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-06-18 15:04:02 +0300

    Update routes

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -31,7 +31,7 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::group([
-    'middleware'=> ['auth:sanctum', 'checkTokenExpiry']
+    'middleware' => ['auth:sanctum', 'checkTokenExpiry']
 ], function () {
     Route::get('profile', [AuthController::class, 'profile']);
     Route::get('logout', [AuthController::class, 'logout']);
@@ -41,64 +41,57 @@ Route::prefix('users')->group(function () {
     Route::get('/', [UserController::class, 'index']);
     Route::delete('/{id}', [UserController::class, 'delete']);
 });
-Route::prefix('report-users')->group(function(){
-    Route::get('/',[ReportUserController::class,'index']);
-    Route::delete('deleteReport/{id}',[ReportUserController::class,'deleteReport']);
-    Route::delete('deleteLandlord/{id}',[ReportUserController::class,'deleteUser']);
+Route::prefix('report-users')->group(function () {
+    Route::get('/', [ReportUserController::class, 'index']);
+    Route::delete('deleteReport/{id}', [ReportUserController::class, 'deleteReport']);
+    Route::delete('deleteLandlord/{id}', [ReportUserController::class, 'deleteUser']);
     Route::post('/', [ReportUserController::class, 'store']);
 });
 
-Route::prefix('report-properties')->group(function(){
-    Route::get('/',[ReportPropertyController::class,'index']);
-    Route::delete('deleteReport/{id}',[ReportPropertyController::class,'deleteReport']);
-    Route::delete('deleteProperty/{id}',[ReportPropertyController::class,'deleteProperty']);
+Route::prefix('report-properties')->group(function () {
+    Route::get('/', [ReportPropertyController::class, 'index']);
+    Route::delete('deleteReport/{id}', [ReportPropertyController::class, 'deleteReport']);
+    Route::delete('deleteProperty/{id}', [ReportPropertyController::class, 'deleteProperty']);
     Route::post('/', [ReportPropertyController::class, 'store']);
-});
-
-Route::prefix('dashboard')->group(function () {
-    Route::get('/counts', [DashboardController::class, 'getCounts']);
 });
 
 Route::apiResource('property-types', PropertyTypeController::class);
 
-Route::prefix('properties')->group(function(){
-    Route::get('/',[PropertyController::class,'index']);
-    Route::get('/{id}',[PropertyController::class,'show']);
-    Route::get('latest-rent/{typeId}',[PropertyController::class,'showLatestRent']);
-    Route::get('latest-sell/{typeId}',[PropertyController::class,'showLatestSell']);
-    Route::post('/',[PropertyController::class,'store']);
-    Route::get('/search/filter',[PropertyController::class,'search']);
+Route::prefix('properties')->group(function () {
+    Route::get('/', [PropertyController::class, 'index']);
+    Route::get('/{id}', [PropertyController::class, 'show']);
+    Route::get('latest-rent/{typeId}', [PropertyController::class, 'showLatestRent']);
+    Route::get('latest-sell/{typeId}', [PropertyController::class, 'showLatestSell']);
+    Route::post('/', [PropertyController::class, 'store']);
+    Route::get('/search/filter', [PropertyController::class, 'search']);
 });
-Route::prefix('notifications')->group(function(){
-    Route::get('/landlord/{landlordId}',[NotificationController::class,'showLandlordNotifications']);
-    Route::get('/renter/{renterId}',[NotificationController::class,'showRenterNotifications']);
-    Route::put('/{id}/type',[NotificationController::class,'updateType']);
+Route::prefix('notifications')->group(function () {
+    Route::get('/landlord/{landlordId}', [NotificationController::class, 'showLandlordNotifications']);
+    Route::get('/renter/{renterId}', [NotificationController::class, 'showRenterNotifications']);
+    Route::put('/{id}/type', [NotificationController::class, 'updateType']);
 });
-Route::prefix('wishlist')->group(function(){
-    Route::get('/{id}',[WishlistController::class,'show']);
-    Route::post('/{id}',[WishlistController::class,'store']);
-});
-
-// Overview Admin Page ROUTE
-Route::prefix('dashboard')->group(function () {
-    Route::get('/home', [DashboardController::class, 'overview']);
-});
-
-// Amenity Admin Page ROUTE
-Route::prefix('amenities')->group(function () {
-    Route::get('/', [AmenityController::class, 'index']);
-    Route::post('/', [AmenityController::class, 'store']);
-    Route::get('/{slug}', [AmenityController::class, 'show']);
-    Route::put('/{slug}', [AmenityController::class, 'update']);
-    Route::delete('/{slug}', [AmenityController::class, 'destroy']);
+Route::prefix('wishlist')->group(function () {
+    Route::get('/{id}', [WishlistController::class, 'show']);
+    Route::post('/{id}', [WishlistController::class, 'store']);
 });
 
 // Admin Dashboard Routes
 Route::group([
-    'middleware'=> ['auth:sanctum', 'checkTokenExpiry', 'admin']
+    'middleware' => ['auth:sanctum', 'checkTokenExpiry', 'admin']
 ], function () {
     Route::prefix('admin')->group(function () {
+        // Overview Admin Page Routes
+        Route::prefix('dashboard')->group(function () {
+            Route::get('/home', [DashboardController::class, 'overview']);
+        });
 
+        // Amenity Admin Page Routes
+        Route::prefix('amenities')->group(function () {
+            Route::get('/', [AmenityController::class, 'index']);
+            Route::post('/', [AmenityController::class, 'store']);
+            Route::get('/{slug}', [AmenityController::class, 'show']);
+            Route::put('/{slug}', [AmenityController::class, 'update']);
+            Route::delete('/{slug}', [AmenityController::class, 'destroy']);
+        });
     });
-
 });
