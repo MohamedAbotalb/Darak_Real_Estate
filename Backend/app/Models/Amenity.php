8387dd9e2e22d0e4ee62be4d264e6f274e1f90dc
dohaseif2 <dohaseif2@gmail.com>
2024-06-23 17:55:07 +0300

    fix: create and update slug automatic when create new(amenity , property)

--- Backend/app/Models/Amenity.php
+++ Backend/app/Models/Amenity.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Amenity extends Model
 {
@@ -13,5 +14,16 @@ class Amenity extends Model
     public function properties()
     {
         return $this->hasMany(Property::class, 'property_amenities');
+    }
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($amenity) {
+            $amenity->slug = Str::slug($amenity->name);
+        });
+        static::updating(function ($amenity) {
+            $amenity->slug = Str::slug($amenity->name);
+        });
     }
 }
