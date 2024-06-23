625d7e863edf53d8491eae39040aade12b09231b
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-23 23:20:38 +0300

    Add social fields in the fillable values in users model

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -24,7 +24,9 @@ class User extends Authenticatable
         'password',
         'phone_number',
         'avatar',
-        'role'
+        'role',
+        'social_id',
+        'social_type',
     ];
 
     /**
