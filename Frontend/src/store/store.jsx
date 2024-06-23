f86af71bdf4d067e8cf5194848e117e0418c6a9e
fatmamahmoud30114 <fatmamahmoud30114@gmail.com>
2024-06-23 14:15:03 +0200

    property_details

--- Frontend/src/store/store.jsx
+++ Frontend/src/store/store.jsx
@@ -2,11 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reportUsersReducer from './reportUsersSlice';
 import reportPropertiesReducer from './reportPropertiesSlice';
+import propertyReducer from './propertySlice';
 
 const reduxStore = configureStore({
   reducer: {
     reportUsers: reportUsersReducer,
     reportProperties: reportPropertiesReducer,
+    property: propertyReducer,
   },
 });
 
