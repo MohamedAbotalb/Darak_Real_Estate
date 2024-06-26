dbc5fef7cae0d0c36cccbe1d4ec70bedcd103974
naglaasaad2000 <naglaasaadmohamed@gmail.com>
2024-06-26 15:10:35 +0200

    about

--- Frontend/src/components/Home/About.jsx
+++ Frontend/src/components/Home/About.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Container, Grid } from '@mui/material';
 
-function About() {
+const AboutUs = () => {
   return (
-    <div className="about-section">
-      <Typography variant="h5" gutterBottom>
-        About us
-      </Typography>
-      <Typography variant="body1" paragraph>
-        Labore proident nisi fugiat nostrud sint mollit aliqua ipsum ad veniam
-        cupidatat ullamco ullamco et. Aliqua tempor do consectetur reprehenderit
-        Lorem aliqua commodo occaecat deserunt. Do eiusmod incididunt.
-      </Typography>
-      <Button variant="contained" color="primary">
-        Learn more
-      </Button>
-    </div>
+    <Container sx={{ my: 8 }}>
+      <Grid container spacing={4} alignItems="center">
+        <Grid item xs={12} md={6}>
+          <Typography variant="h4" gutterBottom>
+            About us
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Labore proident nisi fugiat nostrud sint mollit aliqua ipsum ad veniam cupidatat ullamco ullamco et.
+            Aliqua tempor do consectetur reprehenderit Lorem aliqua commodo occaecat deserunt. Do eiusmod incididunt.
+          </Typography>
+          <Button variant="contained" color="primary">
+            Learn more
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box
+            component="img"
+            src="https://via.placeholder.com/400" // Replace with the actual image source
+            alt="About Us"
+            sx={{ width: '100%', borderRadius: 2 }}
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
-}
+};
 
-export default About;
+export default AboutUs;
