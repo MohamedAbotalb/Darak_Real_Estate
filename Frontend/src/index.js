f515f53d9a8024a1c2ae572c8e28854f67a5d217
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-26 14:10:01 +0300

    Update index.js file

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,11 +1,9 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import reduxStore from './store/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
