b8dd8b9fd6ec6190700b22e93aab31e00c431e02
naglaasaad2000 <naglaasaadmohamed@gmail.com>
2024-06-26 17:48:34 +0200

    navbar and about

--- Frontend/src/components/Home/About.jsx
+++ Frontend/src/components/Home/About.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Box, Typography, Button, Container, Grid } from '@mui/material';
 
-function AboutUs() {
+const AboutUs = () => {
   return (
     <Container sx={{ my: 8 }}>
       <Grid container spacing={4} alignItems="center">
         <Grid item xs={12} md={6}>
+          <Typography variant="overline" display="block" gutterBottom>
+            Dolore dolore voluptate aliqua ut mi
+          </Typography>
           <Typography variant="h4" gutterBottom>
             About us
+            <Box
+              component="span"
+              sx={{
+                display: 'block',
+                width: '50px',
+                height: '4px',
+                backgroundColor: 'primary.main',
+                mt: 1,
+                borderRadius: '2px',
+              }}
+            />
           </Typography>
           <Typography variant="body1" paragraph>
-            Labore proident nisi fugiat nostrud sint mollit aliqua ipsum ad
-            veniam cupidatat ullamco ullamco et. Aliqua tempor do consectetur
-            reprehenderit Lorem aliqua commodo occaecat deserunt. Do eiusmod
-            incididunt.
+            Labore proident nisi fugiat nostrud sint mollit aliqua ipsum ad veniam cupidatat ullamco ullamco et.
+            Aliqua tempor do consectetur reprehenderit Lorem aliqua commodo occaecat deserunt. Do eiusmod incididunt.
           </Typography>
           <Button variant="contained" color="primary">
             Learn more
@@ -21,15 +33,43 @@ function AboutUs() {
         </Grid>
         <Grid item xs={12} md={6}>
           <Box
-            component="img"
-            src="https://via.placeholder.com/400" // Replace with the actual image source
-            alt="About Us"
-            sx={{ width: '100%', borderRadius: 2 }}
-          />
+            sx={{
+              position: 'relative',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 0,
+                left: '25%',
+                width: '50%',
+                height: '100%',
+                borderRadius: '0 50% 50% 0',
+                backgroundColor: '#f0f4fa',
+                zIndex: -1,
+              }}
+            />
+            <Box
+              component="img"
+              src="https://via.placeholder.com/400"
+              alt="About Us"
+              sx={{
+                width: '70%',
+                height: '70%',
+                objectFit: 'cover',
+                borderRadius: 2,
+              }}
+            />
+          </Box>
         </Grid>
       </Grid>
     </Container>
   );
-}
+};
 
 export default AboutUs;
--- Frontend/src/components/Home/Footer.jsx
+++ Frontend/src/components/Home/Footer.jsx
@@ -1,51 +1,74 @@
 // src/components/Footer.jsx
 import React from 'react';
-import { Box, Typography, TextField, Button, IconButton } from '@mui/material';
+import { Box, Typography, TextField, IconButton, Grid, InputAdornment } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import SendIcon from '@mui/icons-material/Send';
 import './Footer.css';
 
 function Footer() {
+  const handleSubscribe = () => {
+    // Add subscribe logic here
+    console.log('Subscribing...');
+  };
+
   return (
     <Box className="footer">
-      <Typography variant="h6" gutterBottom>
-        Real Estate
-      </Typography>
-      <Box className="footer-links">
-        <IconButton color="inherit" href="#">
-          <FacebookIcon />
-        </IconButton>
-        <IconButton color="inherit" href="#">
-          <TwitterIcon />
-        </IconButton>
-        <IconButton color="inherit" href="#">
-          <LinkedInIcon />
-        </IconButton>
-        <IconButton color="inherit" href="#">
-          <InstagramIcon />
-        </IconButton>
-      </Box>
-      <Typography variant="body2" gutterBottom>
-        Register now to get updates on promotions.
-      </Typography>
-      <Box className="subscription">
-        <TextField
-          label="Enter Email Address"
-          variant="outlined"
-          size="small"
-          className="subscription-input"
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          className="subscribe-button"
-        >
-          Subscribe
-        </Button>
-      </Box>
-      <Typography variant="body2" className="footer-note">
+      <Grid container spacing={6} justifyContent="center">
+        <Grid item xs={12} sm={6}>
+          <Box >
+            <Typography variant="h6" gutterBottom>
+              Real Estate
+            </Typography>
+             <Typography variant="body2" gutterBottom textAlign="left">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nisi excepturi aliquam sint quod corrupti id obcaecati, consequuntur natus nulla impedit tempore sapiente ut eos dolore veritatis quaerat beatae animi.
+            </Typography>
+            <Box display="flex" justifyContent="left" marginBottom={2}>
+              <IconButton color="inherit" href="#">
+                <FacebookIcon />
+              </IconButton>
+              <IconButton color="inherit" href="#">
+                <TwitterIcon />
+              </IconButton>
+              <IconButton color="inherit" href="#">
+                <LinkedInIcon />
+              </IconButton>
+              <IconButton color="inherit" href="#">
+                <InstagramIcon />
+              </IconButton>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box alignItems="center" justifyContent="center">
+             <Typography variant="body2" gutterBottom>
+              Register now to get updates on promotions.
+            </Typography>
+            <TextField
+              label="Enter Email Address"
+              variant="outlined"
+              size="small"
+              className="subscription-input"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      color="primary"
+                      aria-label="subscribe"
+                      onClick={handleSubscribe}
+                    >
+                      <SendIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+        </Grid>
+      </Grid>
+      <Typography variant="body2" className="footer-note" align="center" style={{ marginTop: '20px' }}>
         All Rights Reserved © 2024 Real Estate
       </Typography>
     </Box>
