e0be468d9ad22a89148fe74fa46591c1a3b739aa
dohaseif2 <dohaseif2@gmail.com>
2024-06-28 18:17:47 +0300

    refactor: apply repository design pattern in wishlist

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Repositories\UserRepository;
 use App\Repositories\UserRepositoryInterface;
+use App\Repositories\WishlistRepository;
+use App\Repositories\WishlistRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(WishlistRepositoryInterface::class, WishlistRepository::class);
 
     }
 
