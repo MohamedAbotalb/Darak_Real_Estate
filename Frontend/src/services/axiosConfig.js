bde66c4acbf270fbf19095216df1457d452f15ff
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-29 13:22:12 +0300

    Update axiosConfig file

--- Frontend/src/services/axiosConfig.js
+++ Frontend/src/services/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import secureLocalStorage from 'react-secure-storage';
 
-// Function to get the CSRF token from meta tag
+// Get the CSRF token from meta tag
 const getCsrfToken = () => {
   const meta = document.querySelector('meta[name="csrf-token"]');
   return meta ? meta.getAttribute('content') : null;
@@ -11,23 +12,36 @@ const fetchCsrfToken = async () => {
   await axios.get('http://localhost:8000/sanctum/csrf-cookie');
 };
 
+// Get token from localStorage
+const getToken = () => {
+  return secureLocalStorage.getItem('token');
+};
+
 // Create Axios instance
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:8000/api',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': getCsrfToken(),
+    Authorization: `Bearer ${getToken()}`,
   },
   withCredentials: true,
 });
 
 // Request interceptor to attach the CSRF token
 axiosInstance.interceptors.request.use(async (config) => {
-  await fetchCsrfToken(); // Ensure CSRF token is fetched before each request
+  await fetchCsrfToken();
   const token = getCsrfToken();
   if (token) {
     config.headers['X-CSRF-TOKEN'] = token;
-    config.headers.Authorization = `Bearer ${token}`;
   }
+
+  // Fetch updated token if available
+  const updatedToken = getToken();
+  if (updatedToken) {
+    config.headers.Authorization = `Bearer ${updatedToken}`;
+  }
+
   return config;
 });
 
