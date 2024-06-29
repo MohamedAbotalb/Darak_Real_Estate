51eebf6e2bba786a82ac36be727489df135e905f
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-06-29 03:50:05 +0300

    Add Profile Page Route

--- Frontend/src/services/userDetails.js
+++ Frontend/src/services/userDetails.js
@@ -1,3 +1,4 @@
+import secureLocalStorage from 'react-secure-storage';
 import axios from '../axiosConfig';
 
 const fetchUsers = async () => {
@@ -10,4 +11,30 @@ const deleteUser = async (userId) => {
   return userId;
 };
 
-export { fetchUsers, deleteUser };
+const getUser = async () => {
+  const response = await axios.get('/api/profile', {
+    headers: {
+      Authorization: `Bearer ${secureLocalStorage.getItem('token')}`,
+    },
+  });
+  return response;
+};
+
+const updateUser = async (data) => {
+  const response = await axios.put('/api/users/update', data, {
+    headers: {
+      Authorization: `Bearer ${secureLocalStorage.getItem('token')}`,
+    },
+  });
+  return response;
+};
+
+const updatePassword = async (data) => {
+  const response = await axios.put('/api/users/updatePassword', data, {
+    headers: {
+      Authorization: `Bearer ${secureLocalStorage.getItem('token')}`,
+    },
+  });
+  return response;
+};
+export { fetchUsers, deleteUser, getUser, updateUser, updatePassword };
