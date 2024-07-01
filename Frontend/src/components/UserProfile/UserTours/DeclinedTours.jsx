3918e4e364c34ca70b1cd2d31b16a7672743fbd5
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-07-01 23:24:03 +0300

    Add User Tours Components

---
+++ Frontend/src/components/UserProfile/UserTours/DeclinedTours.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function DeclinedTours() {
+  return (
+    <div>
+      <Typography variant="h6">Declined Tours</Typography>
+      <Typography>
+        This is where the content for declined tours will be displayed.
+      </Typography>
+    </div>
+  );
+}
+
+export default DeclinedTours;
