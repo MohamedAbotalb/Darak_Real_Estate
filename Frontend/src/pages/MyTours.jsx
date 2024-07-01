02d243b354d2d2fae8d0177db1e39f9c83e27587
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-07-01 23:23:26 +0300

    Add My Tours Page

---
+++ Frontend/src/pages/MyTours.jsx
@@ -0,0 +1,12 @@
+import Header from 'components/Home/Header';
+import CenteredTabs from 'components/UserProfile/UserTours/CenteredTabs';
+import React from 'react';
+
+export default function MyTours() {
+  return (
+    <div>
+      <Header />
+      <CenteredTabs />
+    </div>
+  );
+}
