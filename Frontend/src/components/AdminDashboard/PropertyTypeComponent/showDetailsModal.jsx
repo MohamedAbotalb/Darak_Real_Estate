2fede3ad9642126fe3ab67dbf1df005b2f5099ca
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-07-02 21:55:15 +0300

    Update File Imports

---
+++ Frontend/src/components/AdminDashboard/PropertyTypeComponent/showDetailsModal.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  Modal,
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+} from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { fetchPropertyTypeDetails } from 'store/propertyTypesSlice';
+
+function ShowDetailsModal({ typeSlug, isOpen, handleClose }) {
+  const dispatch = useDispatch();
+  const type = useSelector((state) => state.propertyTypes.propertyTypeDetails);
+
+  useEffect(() => {
+    if (isOpen && typeSlug) {
+      dispatch(fetchPropertyTypeDetails(typeSlug));
+    }
+  }, [dispatch, isOpen, typeSlug]);
+
+  return (
+    <Modal open={isOpen} onClose={handleClose}>
+      <Box
+        sx={{
+          width: '50%',
+          p: 4,
+          margin: 'auto',
+          mt: '5%',
+          bgcolor: 'background.paper',
+          borderRadius: 1,
+          boxShadow: 24,
+          maxHeight: '80vh',
+          overflowY: 'auto',
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Property Type Details
+        </Typography>
+        {type ? (
+          <>
+            <Typography variant="body1" gutterBottom>
+              Name: {type.name}
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              Properties:
+            </Typography>
+            {type.properties && type.properties.length > 0 ? (
+              type.properties.map((property) => (
+                <Card key={property.slug} sx={{ mb: 2 }}>
+                  <CardContent>
+                    <Typography variant="h6" component="div">
+                      {property.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {property.description}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Rooms: {property.num_of_rooms} | Bathrooms:{' '}
+                      {property.num_of_bathrooms} | Area: {property.area} m²
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Price: ${property.price}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Availability: {property.availability}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Listing Type: {property.listing_type}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View Details
+                    </Button>
+                  </CardActions>
+                </Card>
+              ))
+            ) : (
+              <Typography variant="body2">No properties available.</Typography>
+            )}
+          </>
+        ) : (
+          <Typography variant="body2">Loading...</Typography>
+        )}
+      </Box>
+    </Modal>
+  );
+}
+
+ShowDetailsModal.propTypes = {
+  typeSlug: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+};
+
+export default ShowDetailsModal;
---
+++ Frontend/src/components/AdminDashboard/ReviewList.jsx
@@ -0,0 +1,178 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import {
+  Box,
+  Typography,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  InputBase,
+} from '@mui/material';
+import GridOnIcon from '@mui/icons-material/GridOn';
+import SearchIcon from '@mui/icons-material/Search';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.grey[700],
+    color: theme.palette.common.white,
+    textAlign: 'center',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    textAlign: 'center',
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: '20%',
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function ReviewList() {
+  const [reviews, setReviews] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const fetchReviews = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api/reviews');
+      if (!response.ok) {
+        throw new Error('Failed to fetch reviews');
+      }
+      const data = await response.json();
+      setReviews(data);
+    } catch (error) {
+      setFetchError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredReviews = reviews.filter((review) =>
+    review.property.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          mb: 4,
+          px: 2,
+          py: 2,
+          backgroundColor: '#E8DFDE',
+          borderRadius: 1,
+          boxShadow: '0 1px 3px rgba(0, 0, 0, 0.1)',
+        }}
+      >
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <GridOnIcon sx={{ mr: 1, color: 'black' }} />
+          <Typography variant="h5" sx={{ fontWeight: 'bold', color: 'black' }}>
+            Reviews
+          </Typography>
+        </Box>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search Property"
+            inputProps={{ 'aria-label': 'search property' }}
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </Search>
+      </Box>
+      {fetchError && <div>Error: {fetchError}</div>}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>ID</StyledTableCell>
+              <StyledTableCell align="center">User</StyledTableCell>
+              <StyledTableCell align="center">Property</StyledTableCell>
+              <StyledTableCell align="center">Content</StyledTableCell>
+              <StyledTableCell align="center">Rate</StyledTableCell>
+              <StyledTableCell align="center">Date</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredReviews.map((review) => (
+              <StyledTableRow key={review.id}>
+                <StyledTableCell component="th" scope="row">
+                  {review.id}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {`${review.user.first_name} ${review.user.last_name}`}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {review.property.title}
+                </StyledTableCell>
+                <StyledTableCell align="center">{` ${review.content}`}</StyledTableCell>
+                <StyledTableCell align="center">{review.rate}</StyledTableCell>
+                <StyledTableCell align="center">
+                  {new Date(review.date).toLocaleDateString()}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
