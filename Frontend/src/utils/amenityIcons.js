4f0ca8a700e3e09201169c87d6b6a3a2c520d887
fatmamahmoud30114 <fatmamahmoud30114@gmail.com>
2024-07-03 13:09:26 +0200

    amenityIcone_file

---
+++ Frontend/src/utils/amenityIcons.js
@@ -0,0 +1,15 @@
+import GymIcon from '@mui/icons-material/FitnessCenter';
+import PoolIcon from '@mui/icons-material/Pool';
+import WifiIcon from '@mui/icons-material/Wifi';
+import ParkingIcon from '@mui/icons-material/LocalParking';
+import SpaIcon from '@mui/icons-material/Spa';
+
+const amenityIcons = {
+  gym: GymIcon,
+  pool: PoolIcon,
+  wifi: WifiIcon,
+  parking: ParkingIcon,
+  spa: SpaIcon,
+};
+
+export default amenityIcons;
