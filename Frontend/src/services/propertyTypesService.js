d9d231e621fb29bb419abdca10fdd2d021e1ab83
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-07-04 06:08:54 +0300

    Update propertyTypeService file

--- Frontend/src/services/propertyTypesService.js
+++ Frontend/src/services/propertyTypesService.js
@@ -1,57 +1,14 @@
 import axios from 'services/axiosConfig';
-import { toast } from 'react-toastify';
 
-const getAllPropertyTypes = async () => {
-  const response = await axios.get('/property-types');
-  return response.data;
-};
+export const getAllPropertyTypes = () => axios.get('/property-types');
 
-const getPropertyTypeBySlug = async (typeSlug) => {
-  try {
-    const response = await axios.get(`/property-types/${typeSlug}`);
-    return response.data;
-  } catch (error) {
-    toast.error('Failed to fetch property type details.');
-    throw error;
-  }
-};
+export const getPropertyTypeBySlug = (typeSlug) =>
+  axios.get(`/property-types/${typeSlug}`);
 
-const addPropertyType = async (data) => {
-  try {
-    const response = await axios.post('/property-types', data);
-    toast.success('Property type added successfully!');
-    return response.data;
-  } catch (error) {
-    toast.error('Failed to add property type.');
-    throw error;
-  }
-};
+export const addPropertyType = (data) => axios.post('/property-types', data);
 
-const editPropertyType = async (slug, data) => {
-  try {
-    const response = await axios.put(`/property-types/${slug}`, data);
-    toast.success('Property type updated successfully!');
-    return response.data;
-  } catch (error) {
-    toast.error('Failed to update property type.');
-    throw error;
-  }
-};
+export const editPropertyType = (slug, data) =>
+  axios.put(`/property-types/${slug}`, data);
 
-const deletePropertyType = async (slug) => {
-  try {
-    await axios.delete(`/property-types/${slug}`);
-    toast.success('Property type deleted successfully!');
-  } catch (error) {
-    toast.error('Failed to delete property type.');
-    throw error;
-  }
-};
-
-export {
-  getAllPropertyTypes,
-  getPropertyTypeBySlug,
-  addPropertyType,
-  editPropertyType,
-  deletePropertyType,
-};
+export const deletePropertyType = (slug) =>
+  axios.delete(`/property-types/${slug}`);
