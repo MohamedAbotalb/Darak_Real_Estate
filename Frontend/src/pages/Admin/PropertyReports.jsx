7d6f5059e57beb3a859497a38e27e09a095f203b
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-07-05 04:12:48 +0300

    Add Admin PropertyReports Page

---
+++ Frontend/src/pages/Admin/PropertyReports.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReportPropertyList from 'components/AdminDashboard/ReportPropertyList';
+
+function PropertyReportsPage() {
+  return <ReportPropertyList />;
+}
+
+export default PropertyReportsPage;
