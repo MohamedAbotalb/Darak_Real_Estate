cc305af4e35a1e56ac9f5a3f10216a9fa00e954c
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-07-05 16:58:30 +0300

    Add Advanced Search Form Component

--- Frontend/src/components/PropertySearch/SearchForm.jsx
+++ Frontend/src/components/PropertySearch/SearchForm.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
 import {
   Box,
   IconButton,
@@ -10,22 +11,22 @@ import {
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { styled } from '@mui/system';
+import { toast } from 'react-toastify';
 import { fetchPropertyTypes } from 'store/home/propertyTypeSlice';
 import { fetchLocations } from 'store/home/locationsSlice';
 import { fetchProperties } from 'store/propertySearchSlice';
 
-const SearchContainer = styled(Box)(({ theme }) => ({
+const SearchContainer = styled(Box)(() => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  gap: theme.spacing(2),
   flexWrap: 'wrap',
   margin: '20px auto',
 }));
 
-const SearchFormControl = styled(FormControl)(({ theme }) => ({
-  minWidth: '130px',
-  marginRight: theme.spacing(1),
+const SearchFormControl = styled(FormControl)(() => ({
+  minWidth: '120px',
+  marginRight: '10px',
   '& .MuiInputBase-root': {
     backgroundColor: '#fff',
     color: '#000',
@@ -36,7 +37,6 @@ const SearchFormControl = styled(FormControl)(({ theme }) => ({
 const SearchButton = styled(IconButton)({
   height: '56px',
   width: '56px',
-  padding: '0 16px',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
@@ -50,16 +50,17 @@ const SearchButton = styled(IconButton)({
   },
 });
 
-function PropertySearch() {
+function SearchForm() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const { propertyTypes } = useSelector((state) => state.propertyTypes || []);
   const locations = useSelector((state) => state.locations.data || []);
   const locationsStatus = useSelector((state) => state.locations.status);
-  const propertyTypesStatus = useSelector(
-    (state) => state.propertyTypes.status
-  );
+  const typesStatus = useSelector((state) => state.propertyTypes.status);
 
-  const [rentOrBuy, setRentOrBuy] = useState('');
+  const [listingType, setListingType] = useState('');
   const [propertyType, setPropertyType] = useState('');
   const [city, setCity] = useState('');
   const [bedrooms, setBedrooms] = useState('');
@@ -67,20 +68,75 @@ function PropertySearch() {
   const [minPrice, setMinPrice] = useState('');
   const [maxPrice, setMaxPrice] = useState('');
 
-  const isLoading =
-    locationsStatus === 'loading' || propertyTypesStatus === 'loading';
+  const isLoading = locationsStatus === 'loading' || typesStatus === 'loading';
 
   useEffect(() => {
     dispatch(fetchPropertyTypes());
     dispatch(fetchLocations());
-  }, [dispatch]);
+
+    // Set the component state according to URL parameters
+    const urlParams = new URLSearchParams(location.search);
+    const lt = urlParams.get('lt') || '';
+    const pt = urlParams.get('pt') || '';
+    const c = urlParams.get('c') || '';
+    const bdr = urlParams.get('bdr') || '';
+    const btr = urlParams.get('btr') || '';
+    const mnp = urlParams.get('mnp') || '';
+    const mxp = urlParams.get('mxp') || '';
+
+    setListingType(lt);
+    setPropertyType(pt);
+    setCity(c);
+    setBedrooms(bdr);
+    setBathrooms(btr);
+    setMinPrice(mnp);
+    setMaxPrice(mxp);
+
+    // Fetch properties based on URL parameters
+    dispatch(
+      fetchProperties({
+        propertyType: pt,
+        city: c,
+        listingType: lt,
+        bedrooms: bdr,
+        bathrooms: btr,
+        minPrice: mnp,
+        maxPrice: mxp,
+      })
+    );
+  }, [dispatch, location.search]);
 
   const handleSearch = () => {
+    if (parseInt(minPrice, 10) > parseInt(maxPrice, 10)) {
+      toast.error('Min price should be less than or equal to max price');
+      return;
+    }
+
+    // Create query object with only the chosen filters
+    const query = {};
+    if (listingType) query.lt = listingType;
+    if (propertyType) query.pt = propertyType;
+    if (city) query.c = city;
+    if (bedrooms) query.bdr = bedrooms;
+    if (bathrooms) query.btr = bathrooms;
+    if (minPrice) query.mnp = minPrice;
+    if (maxPrice) query.mxp = maxPrice;
+
+    // Convert query object to URL parameters
+    const searchParams = new URLSearchParams(query).toString();
+
+    // Update URL with search parameters
+    navigate({
+      pathname: '/properties',
+      search: searchParams,
+    });
+
+    // Fetch properties based on filters
     dispatch(
       fetchProperties({
         propertyType,
         city,
-        listingType: rentOrBuy,
+        listingType,
         bedrooms,
         bathrooms,
         minPrice,
@@ -89,23 +145,61 @@ function PropertySearch() {
     );
   };
 
+  // Price options based on listingType selection
+  const rentPriceOptions = [
+    5000, 10000, 15000, 20000, 25000, 30000, 40000, 50000, 60000, 70000, 80000,
+    90000, 100000,
+  ];
+
+  const buyPriceOptions = [
+    100000, 200000, 300000, 400000, 500000, 1000000, 2000000, 5000000, 10000000,
+    30000000,
+  ];
+
+  const priceOptions =
+    listingType === 'rent' ? rentPriceOptions : buyPriceOptions;
+
+  // Filtered price options based on minPrice and maxPrice
+  const filteredMinPriceOptions = priceOptions.filter(
+    (price) => !maxPrice || price <= parseInt(maxPrice, 10)
+  );
+
+  const filteredMaxPriceOptions = priceOptions.filter(
+    (price) => !minPrice || price >= parseInt(minPrice, 10)
+  );
+
+  const menuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: 200,
+      },
+    },
+  };
+
   return (
     <SearchContainer>
       {isLoading ? (
-        <CircularProgress size={50} />
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+        >
+          <CircularProgress size={50} />
+        </Box>
       ) : (
         <>
           <SearchFormControl variant="outlined">
             <Select
-              value={rentOrBuy}
-              onChange={(e) => setRentOrBuy(e.target.value)}
+              value={listingType}
+              onChange={(e) => setListingType(e.target.value)}
               displayEmpty
             >
               <MenuItem value="" disabled>
                 <em>Rent, Buy</em>
               </MenuItem>
-              <MenuItem value="renting">Rent</MenuItem>
-              <MenuItem value="selling">Buy</MenuItem>
+              <MenuItem value="rent">Rent</MenuItem>
+              <MenuItem value="buy">Buy</MenuItem>
             </Select>
           </SearchFormControl>
 
@@ -114,13 +208,14 @@ function PropertySearch() {
               value={city}
               onChange={(e) => setCity(e.target.value)}
               displayEmpty
+              MenuProps={menuProps}
             >
-              <MenuItem value="" disabled>
+              <MenuItem value="">
                 <em>Location</em>
               </MenuItem>
-              {locations.map((location) => (
-                <MenuItem key={location.id} value={location.city}>
-                  {location.city}
+              {locations.map((loc) => (
+                <MenuItem key={loc.id} value={loc.city}>
+                  {loc.city}
                 </MenuItem>
               ))}
             </Select>
@@ -132,7 +227,7 @@ function PropertySearch() {
               onChange={(e) => setPropertyType(e.target.value)}
               displayEmpty
             >
-              <MenuItem value="" disabled>
+              <MenuItem value="">
                 <em>Property Type</em>
               </MenuItem>
               {propertyTypes.map((type) => (
@@ -148,15 +243,17 @@ function PropertySearch() {
               value={bedrooms}
               onChange={(e) => setBedrooms(e.target.value)}
               displayEmpty
+              MenuProps={menuProps}
             >
-              <MenuItem value="" disabled>
+              <MenuItem value="">
                 <em>Bedrooms</em>
               </MenuItem>
-              <MenuItem value="1">1</MenuItem>
-              <MenuItem value="2">2</MenuItem>
-              <MenuItem value="3">3</MenuItem>
-              <MenuItem value="4">4</MenuItem>
-              <MenuItem value="5">5</MenuItem>
+              {[1, 2, 3, 4, 5, 6, 7].map((opt) => (
+                <MenuItem key={opt} value={opt}>
+                  {opt}
+                </MenuItem>
+              ))}
+              <MenuItem value="+7">7+</MenuItem>
             </Select>
           </SearchFormControl>
 
@@ -165,15 +262,17 @@ function PropertySearch() {
               value={bathrooms}
               onChange={(e) => setBathrooms(e.target.value)}
               displayEmpty
+              MenuProps={menuProps}
             >
-              <MenuItem value="" disabled>
+              <MenuItem value="">
                 <em>Bathrooms</em>
               </MenuItem>
-              <MenuItem value="1">1</MenuItem>
-              <MenuItem value="2">2</MenuItem>
-              <MenuItem value="3">3</MenuItem>
-              <MenuItem value="4">4</MenuItem>
-              <MenuItem value="5">5</MenuItem>
+              {[1, 2, 3, 4, 5, 6, 7].map((opt) => (
+                <MenuItem key={opt} value={opt}>
+                  {opt}
+                </MenuItem>
+              ))}
+              <MenuItem value="+7">7+</MenuItem>
             </Select>
           </SearchFormControl>
 
@@ -182,19 +281,16 @@ function PropertySearch() {
               value={minPrice}
               onChange={(e) => setMinPrice(e.target.value)}
               displayEmpty
+              MenuProps={menuProps}
             >
-              <MenuItem value="" disabled>
+              <MenuItem value="">
                 <em>Min Price</em>
               </MenuItem>
-              <MenuItem value="100000">100,000</MenuItem>
-              <MenuItem value="200000">200,000</MenuItem>
-              <MenuItem value="300000">300,000</MenuItem>
-              <MenuItem value="400000">400,000</MenuItem>
-              <MenuItem value="500000">500,000</MenuItem>
-              <MenuItem value="1000000">1,000,000</MenuItem>
-              <MenuItem value="2000000">2,000,000</MenuItem>
-              <MenuItem value="5000000">5,000,000</MenuItem>
-              <MenuItem value="10000000">10,000,000</MenuItem>
+              {filteredMinPriceOptions.map((price) => (
+                <MenuItem key={price} value={price}>
+                  {price.toLocaleString()}
+                </MenuItem>
+              ))}
             </Select>
           </SearchFormControl>
 
@@ -203,19 +299,16 @@ function PropertySearch() {
               value={maxPrice}
               onChange={(e) => setMaxPrice(e.target.value)}
               displayEmpty
+              MenuProps={menuProps}
             >
-              <MenuItem value="" disabled>
+              <MenuItem value="">
                 <em>Max Price</em>
               </MenuItem>
-              <MenuItem value="100000">100,000</MenuItem>
-              <MenuItem value="200000">200,000</MenuItem>
-              <MenuItem value="300000">300,000</MenuItem>
-              <MenuItem value="400000">400,000</MenuItem>
-              <MenuItem value="500000">500,000</MenuItem>
-              <MenuItem value="1000000">1,000,000</MenuItem>
-              <MenuItem value="2000000">2,000,000</MenuItem>
-              <MenuItem value="5000000">5,000,000</MenuItem>
-              <MenuItem value="10000000">10,000,000</MenuItem>
+              {filteredMaxPriceOptions.map((price) => (
+                <MenuItem key={price} value={price}>
+                  {price.toLocaleString()}
+                </MenuItem>
+              ))}
             </Select>
           </SearchFormControl>
 
@@ -232,4 +325,4 @@ function PropertySearch() {
   );
 }
 
-export default PropertySearch;
+export default SearchForm;
