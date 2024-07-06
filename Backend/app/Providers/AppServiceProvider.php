d16aa9cc9040cf89d65cff780e6249713e861f5d
dohaseif2 <dohaseif2@gmail.com>
2024-07-06 16:38:14 +0300

    refactor: make service provider for repositories

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -1,33 +1,6 @@
 <?php
 
 namespace App\Providers;
-
-use App\Repositories\AmenityRepository;
-use App\Repositories\Contracts\AmenityRepositoryInterface;
-use App\Repositories\ImageRepository;
-use App\Repositories\Contracts\ImageRepositoryInterface;
-use App\Repositories\NotificationRepository;
-use App\Repositories\Contracts\NotificationRepositoryInterface;
-use App\Repositories\PropertyRepository;
-use App\Repositories\Contracts\PropertyRepositoryInterface;
-use App\Repositories\PropertyTypeRepository;
-use App\Repositories\Contracts\PropertyTypeRepositoryInterface;
-use App\Repositories\Contracts\ReactionRepositoryInterface;
-use App\Repositories\ReasonReportRepository;
-use App\Repositories\Contracts\ReasonReportRepositoryInterface;
-use App\Repositories\ReportPropertyRepository;
-use App\Repositories\Contracts\ReportPropertyRepositoryInterface;
-use App\Repositories\ReportUserRepository;
-use App\Repositories\Contracts\ReportUserRepositoryInterface;
-use App\Repositories\ReviewRepository;
-use App\Repositories\Contracts\ReviewRepositoryInterface;
-use App\Repositories\TourRepository;
-use App\Repositories\Contracts\TourRepositoryInterface;
-use App\Repositories\UserRepository;
-use App\Repositories\Contracts\UserRepositoryInterface;
-use App\Repositories\WishlistRepository;
-use App\Repositories\Contracts\WishlistRepositoryInterface;
-use App\Repositories\ReactionRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -37,19 +10,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-        $this->app->bind(WishlistRepositoryInterface::class, WishlistRepository::class);
-        $this->app->bind(PropertyRepositoryInterface::class, PropertyRepository::class);
-        $this->app->bind(NotificationRepositoryInterface::class, NotificationRepository::class);
-        $this->app->bind(TourRepositoryInterface::class, TourRepository::class);
-        $this->app->bind(ReportPropertyRepositoryInterface::class, ReportPropertyRepository::class);
-        $this->app->bind(ReportUserRepositoryInterface::class, ReportUserRepository::class);
-        $this->app->bind(AmenityRepositoryInterface::class, AmenityRepository::class);
-        $this->app->bind(PropertyTypeRepositoryInterface::class, PropertyTypeRepository::class);
-        $this->app->bind(ImageRepositoryInterface::class, ImageRepository::class);
-        $this->app->bind(ReasonReportRepositoryInterface::class, ReasonReportRepository::class);
-        $this->app->bind(ReviewRepositoryInterface::class, ReviewRepository::class);
-        $this->app->bind(ReactionRepositoryInterface::class, ReactionRepository::class);
+        
     }
 
     /**
