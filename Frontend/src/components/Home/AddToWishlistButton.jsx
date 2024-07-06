7cfc606d7118e286f2e4df9fe7606e00ee43c0ac
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-07-07 01:06:43 +0300

    Update AddToWishlist Component

--- Frontend/src/components/Home/AddToWishlistButton.jsx
+++ Frontend/src/components/Home/AddToWishlistButton.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { IconButton } from '@mui/material';
 import { Favorite, FavoriteBorder } from '@mui/icons-material';
-import { toast } from 'react-toastify';
+import { successToast } from 'utils/toast';
 import {
   fetchWishlist,
   addToWishlist,
@@ -26,22 +26,22 @@ function AddToWishlistButton({ property }) {
   );
 
   const handleToggleWishlist = async () => {
-    if (isInWishlist) {
-      const wishlistItem = wishlist.find(
-        (item) => item.property && item.property.id === property.id
-      );
-      dispatch(removeFromWishlist(wishlistItem.id));
-      toast.success('Property removed from wishlist successfully', {
-        position: 'top-right',
-      });
-    } else {
-      dispatch(addToWishlist(property));
-      toast.success('Property added to wishlist successfully', {
-        position: 'top-right',
-      });
+    if (property) {
+      if (isInWishlist) {
+        const wishlistItem = wishlist.find(
+          (item) => item.property && item.property.id === property.id
+        );
+        if (wishlistItem) {
+          dispatch(removeFromWishlist(wishlistItem.id));
+          successToast('Property removed from wishlist successfully');
+        }
+      } else {
+        dispatch(addToWishlist(property));
+        successToast('Property added to wishlist successfully');
+      }
+      // Fetch the updated wishlist
+      dispatch(fetchWishlist());
     }
-    // Fetch the updated wishlist
-    dispatch(fetchWishlist());
   };
 
   return (
