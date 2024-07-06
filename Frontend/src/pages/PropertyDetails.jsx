4ebc37d1557e909611e38e73e29265cfff75a032
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-07-07 01:10:23 +0300

    Add PropertyDetails Page

---
+++ Frontend/src/pages/PropertyDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import PropertyDetails from 'components/PropertyDetails/PropertyDetails';
+
+function PropertyDetailsPage() {
+  return (
+    <Box>
+      <PropertyDetails />
+    </Box>
+  );
+}
+
+export default PropertyDetailsPage;
