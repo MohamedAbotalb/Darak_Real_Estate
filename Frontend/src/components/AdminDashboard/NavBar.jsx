d20413ed3fff592d6694e65c951c93cc4e06c217
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-07-20 14:25:46 +0300

    Update Admin Navbar Component

--- Frontend/src/components/AdminDashboard/NavBar.jsx
+++ Frontend/src/components/AdminDashboard/NavBar.jsx
@@ -22,7 +22,7 @@ export default function NavBar() {
   const handleLogout = () => {
     dispatch(logout());
     setIsLoggedIn(false);
-    navigate('/');
+    navigate('/admin-login');
   };
 
   return (
