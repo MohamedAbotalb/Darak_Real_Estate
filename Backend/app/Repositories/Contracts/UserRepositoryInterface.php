aaf7031b57a069b8df00b804bf58f1ec38d52e10
dohaseif2 <dohaseif2@gmail.com>
2024-07-21 00:46:56 +0300

    refactor: remove user updates to repostory

--- Backend/app/Repositories/Contracts/UserRepositoryInterface.php
+++ Backend/app/Repositories/Contracts/UserRepositoryInterface.php
@@ -7,5 +7,8 @@ interface UserRepositoryInterface
     public function find(int $id);
     public function all();
     public function update(array $attributes);
+    public function updateName($id, array $data);
+    public function updatePassword($id, $newPassword);
+    public function updatePhone($id, $phoneNumber);
     public function delete(int $id);
 }
