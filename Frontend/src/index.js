818c3eb7ae71837d5a58a59d79c8001a220a14f9
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-07-22 02:42:50 +0300

    Update index.js file

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from 'App';
 import reduxStore from 'store/index';
+import Loader from 'components/Loader';
 import 'index.css';
 import './i18n';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,7 +11,9 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={reduxStore}>
-    <App />
-  </Provider>
+  <Suspense fallback={<Loader />}>
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
+  </Suspense>
 );
